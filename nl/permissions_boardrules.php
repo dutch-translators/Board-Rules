a75dfd5192b0ebb8ab84e1b96462fc96bd1cf7b9
David <neurofibromatose@live.nl>
2014-10-31 15:39:14 +0100

    credit lijn conform de regels

--- nl/permissions_boardrules.php
+++ nl/permissions_boardrules.php
@@ -2,8 +2,9 @@
 /**
 *
 * Board Rules extension for the phpBB Forum Software package.
+* Dutch translation by Dutch Translators (https://github.com/dutch-translators)
 *
-* @copyright (c) 2014 phpBB Limited <https://www.phpbb.com>, Dutch Translators <https://github.com/dutch-translators>
+* @copyright (c) 2014 phpBB Limited <https://www.phpbb.com>
 * @license GNU General Public License, version 2 (GPL-2.0)
 *
 */
